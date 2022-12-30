d7ccc3cec6f0ba6bb0b50786d6d5183eac58509c
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-12-30 15:20:15 +0300

    Ödev

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
-export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CHANGE_OPERATION = "CHANGE_OPERATION"
+export const CLEAR_OPERATION = "CLEAR_OPERATION"
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +10,16 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operation) => {
+    return({type:CHANGE_OPERATION, payload: operation})
+}
+
+export const clearDisplay = () => {
+    return (
+        {
+            type:CLEAR_OPERATION
+        }
+    )
 }
